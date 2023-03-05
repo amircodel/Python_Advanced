98cd295e665efc6e6627dd8ed22c91db22fa380d
amircodel <amirali.mohammadi890@gmail.com>
2023-03-05 21:34:00 +0330

    2303052133

--- jadi_4/divar.py
+++ jadi_4/divar.py
@@ -1,3 +1,16 @@
 import requests
-x = requests.get('https://w3schools.com/python/demopage.htm')
-print(x)
+from bs4 import BeautifulSoup
+import re
+x = requests.get('https://divar.ir/s/tehran').text
+soup = BeautifulSoup(x,'html.parser')
+divars = soup.find_all('div',attrs={'class':'kt-post-card__body'})
+lst = []
+res = []
+for i in divars:
+    lst.append(i.get_text(separator=" ").replace(',',''))
+for i in lst:
+    l = re.split(r'\W+',i)
+    if 'توافقی' in l:
+        res.append(i)
+for i in res:
+    print(i)
