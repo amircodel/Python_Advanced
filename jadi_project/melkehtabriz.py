f6978ec8c19f5f800c21b8d951c09c0df4c37fdf
amircodel <amirali.mohammadi890@gmail.com>
2023-03-24 20:12:48 +0430

    2303242012

--- jadi_project/melkehtabriz.py
+++ jadi_project/melkehtabriz.py
@@ -26,7 +26,7 @@ SCROLL_PAUSE_TIME = 2
 last_height = driver.execute_script("return document.body.scrollHeight")
 
 # If you want to limit the number of scroll loads, add a limit here
-scroll_limit = 2
+scroll_limit = 30
 
 count = 0
 while True and count < scroll_limit:
@@ -62,7 +62,6 @@ for lnk in urls:
     del soup
     data = []
     soup = BeautifulSoup(requests.get(lnk).text,'html.parser')
-    # data.extend(re.findall(r'\d+',lnk))
     ex = soup.find_all('div',attrs={'class':'text-infmlk'})
     data.extend([i.text for i in ex])
     if len(ex) == 13:
@@ -74,14 +73,14 @@ for lnk in urls:
     # Pricing data[3] & permeter data[4]
     data[3] = int(''.join(re.findall(r'\d+',data[3])))
     data.insert(4,int(data[3]//data[2]))
-    # Homes per one floor data[5]
-    # Rooms
+    # floor data[5]
+    # Rooms data[6]
     data[6] = int(data[6])
-    # Total floors
+    # Total floors data[7]
     data[7] = int(data[7])
-    # floor of the home
+    # Homes per one floor data[8]
     data[8] = int(data[8])
-    # Buliding year
+    # Buliding year data[9]
     data[9] = int(re.findall(r'\d+',data[9])[0])
     # Situation data[10]
     # Type of building data[11]
@@ -91,12 +90,12 @@ stmt = "SHOW TABLES LIKE 'melkehtabriz'"
 cursor.execute(stmt)
 isthere = cursor.fetchone()
 if not isthere:
-    sql01 = "CREATE TABLE melkehtabriz (Code VARCHAR(255), Area VARCHAR(255), Meterage FLOAT, Pricing INT, PPM INT, HPF VARCHAR(255), Rooms INT, Allfloors INT, Floor INT, old INT, Situation VARCHAR(255), TypeBuilding VARCHAR(255), TypeDocumentry VARCHAR(255));"
+    sql01 = "CREATE TABLE melkehtabriz (Code VARCHAR(255), Area VARCHAR(255), Meterage FLOAT, Pricing INT, PPM INT, Floor VARCHAR(255), Rooms INT, Allfloors INT, HPF INT, old INT, Situation VARCHAR(255), TypeBuilding VARCHAR(255), TypeDocumentry VARCHAR(255));"
     cursor.execute(sql01)
 for home in main:
     sql02 = f"SELECT * FROM melkehtabriz WHERE Code LIKE '{home[0]}';"
     cursor.execute(sql02)
     if cursor.fetchone():
         cursor.execute(f"DELETE FROM melkehtabriz WHERE Code LIKE '{home[0]}';")
-    cursor.execute("INSERT INTO melkehtabriz (Code, Area, Meterage, Pricing, PPM, HPF, Rooms, Allfloors, Floor, old, Situation, TypeBuilding, TypeDocumentry) VALUES (%s, %s, %f, %i, %i, %s, %i, %i, %i, %i, %s, %s, %s);" %home)
+    cursor.execute(f"INSERT INTO melkehtabriz (Code, Area, Meterage, Pricing, PPM, Floor, Rooms, Allfloors, HPF, old, Situation, TypeBuilding, TypeDocumentry) VALUES {home}")
 mydb.commit()
