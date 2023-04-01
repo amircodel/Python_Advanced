a6a17212c9b7982fdbaf787565ad26cc3d10ee2c
Mona Lisa <amirali.mohammadi890@gmail.com>
2023-04-01 17:29:05 +0330

    17280401

---
+++ jadi_project/reportforowner.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import csv
+main = []
+mydb = mysql.connector.connect(
+  host="localhost",
+  port="3307",
+  user="root",
+  password="",
+  database="python"
+)
+cursor = mydb.cursor()
+cursor.execute("SELECT Area, COUNT(Area) FROM `melkehtabriz` GROUP BY Area ORDER BY COUNT(Area) DESC;")
+with open('report.csv', 'w+', encoding='UTF8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['منطقه','تعداد آگهی'])
+    for area, count in cursor:
+        writer.writerow([area,count])
