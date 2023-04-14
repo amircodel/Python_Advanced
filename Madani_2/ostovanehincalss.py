7134ade4be7ee002532807221a771c87a9bc961e
amircodel <amirali.mohammadi890@gmail.com>
2023-04-14 19:41:19 +0330

    2304141941

---
+++ Madani_2/ostovanehincalss.py
@@ -0,0 +1,14 @@
+from math import pi
+class cylinder:
+    def __init__(self,radius,hight):
+        self.radius = radius
+        self.hight = hight
+    def V(self):
+        print(f'V is {(pi * self.radius ** 2 * self.hight):.2f}')
+    def S(self):
+        print(f'S is {((2 * pi * self.radius ** 2 ) + (2 * pi * self.radius * self.hight)):.2f}')
+r = float(input('ENTER RADIUS '))
+h = float(input('ENTER HIGHT '))
+obj1 = cylinder(r, h)
+obj1.V()
+obj1.S()
