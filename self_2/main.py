b98faddbb90113b970a1626e86c0364158a5dcac
Amirali Mohammadi <74957216+amircodel@users.noreply.github.com>
2023-04-16 22:14:13 +0330

    Update main.py

--- self_2/main.py
+++ self_2/main.py
@@ -10,9 +10,12 @@ que = "بـــــابـــــای تـــــو کــــــیـــــ�
 
 @tree.command(name = "papa", description = "generate ComixZone funny and stereotypy Farsi question") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def papa_command(interaction ,user : discord.User):
-    await interaction.response.send_message(f'{re.sub(r'\s+\#d+',r'\@\s+',user)} {que}')
+    user = re.sub(r'\#\d+','',str(user))
+    user = '@' + user
+    user = discord.User(user)
+    await interaction.response.send_message(f'{user} {que}')
 @client.event
 async def on_ready():
     await tree.sync()
     print("Ready!")
-client.run('MTA5NTA0NDY4Mjg0Njg0MzA2MQ.GGQ2bW.UqS9kUQxh55X4bem3iXosg0E8n_QHaPbGqVwLs')
+client.run('MTA5NTA0NDY4Mjg0Njg0MzA2MQ.G-lG2T.4NeJej93VXaf86bEBOmiNgjYqMA3RlMSPRoCCw')
