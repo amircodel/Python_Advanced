eac2aef4db6dd7daf3198fed25e2bd46323fd747
amircodel <amirali.mohammadi890@gmail.com>
2023-04-17 00:18:34 +0330

    2304170018

--- self_2/main.py
+++ self_2/main.py
@@ -1,21 +1,19 @@
 import discord
 from discord import app_commands
-import re
+from typing import Optional
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-que = "بـــــابـــــای تـــــو کــــــیـــــه ؟"
+que = "بـــــابـــــای تـــــو کــــــیـــــه؟ "
 
 @tree.command(name = "papa", description = "generate ComixZone funny and stereotypy Farsi question") #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def papa_command(interaction ,user : discord.User):
-    user = re.sub(r'\#\d+','',str(user))
-    user = '@' + user
-    user = discord.User(user)
-    await interaction.response.send_message(f'{user} {que}')
+async def papa_command(interaction ,user: discord.User ,emoji: Optional[discord.Emoji]):
+    user = user.mention
+    await interaction.response.send_message(f'{user} {que} {emoji}')
 @client.event
 async def on_ready():
     await tree.sync()
     print("Ready!")
-client.run('MTA5NTA0NDY4Mjg0Njg0MzA2MQ.G-lG2T.4NeJej93VXaf86bEBOmiNgjYqMA3RlMSPRoCCw')
+client.run('MTA5NTA0NDY4Mjg0Njg0MzA2MQ.GvNYdD.FyMrmfrpnuhePZ3Va0mbr8k1cmiOCCaaBD7rEs')
