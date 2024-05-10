858c11482476911340b5ab609b342ee86ef92013
amircodel <amirali.mohammadi890@gmail.com>
2024-05-10 18:51:02 +0330

    2024

--- jadi_1/genre.py
+++ jadi_1/genre.py
@@ -1,18 +1,18 @@
 from collections import OrderedDict
 from operator import itemgetter
-qxnar = [0,0,0,0,0,0]
-xanr = ['Horror', 'Romance', 'Comedy', 'History', 'Adventure', 'Action']
+q_genre = [0,0,0,0,0,0]
+genre = ['Horror', 'Romance', 'Comedy', 'History', 'Adventure', 'Action']
 n = int(input())
 for i in range(n):
     nxanr = []
     c = input()
     nx = c.split()
     nx.pop(0)
-    for j in xanr:
+    for j in genre:
         m = nx.count(j)
         nxanr.append(m)
-    qxnar = [x+y for x,y in zip(qxnar,nxanr)]
-q = OrderedDict(zip(xanr,qxnar))
+    q_genre = [x+y for x,y in zip(q_genre,nxanr)]
+q = OrderedDict(zip(genre,q_genre))
 q = sorted(q.items(), key= itemgetter(0),reverse=False)
 for k,v in sorted(q, key= itemgetter(1),reverse=True):
     print('%s : %i'%(k,v))
